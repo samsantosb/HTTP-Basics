1e45ac9964450da374d5fc733ccc6c831a7aba5a
samsantosb <samsantosb@outlook.com>
2022-09-24 22:06:43 -0300

    first commit

---
+++ http.js
@@ -0,0 +1,7 @@
+/*What is HTTP?*/
+/*HTTP is the protocol that governs communications between web browsers and web servers.
+HTTP stands for Hyper Text Transfer Protocol.
+HTTP is the foundation of data communication for the World Wide Web.
+HTTP is a client-server protocol, which means requests are initiated by the recipient
+usually the Web browser.
+HTTP is a stateless protocol, which means that the server does not keep any data*/
